2ccb461585eb93ee07de2ae2aa0b417ab2bb30a1
Marina Mazurova <sleepofnodreaming12@gmail.com>
2016-07-26 20:25:14 +0300

    database_management.py, test_database.py: tests improved^ docs improved.

--- test_database.py
+++ test_database.py
@@ -6,28 +6,42 @@ from pony import orm
 SELF_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-@pytest.yield_fixture(scope="session")
+@pytest.yield_fixture(scope="function")
 def database():
     with orm.db_session:
         yield db
         db.rollback()
 
-
-def newsfeeds() -> ([News], [News]):
-    feeds = []
-    files = ["examples/news_example_1.pickle", "examples/news_example_2.pickle"]
-    examples = list(map(lambda a: os.path.join(SELF_PATH, a), files))
-    for i in examples:
-        with open(i, "rb") as f:
-            data = pickle.load(f)
-            feeds.append(data)
-    return tuple(feeds)
+@pytest.fixture(scope="session")
+def newsfeeds() -> [([News, News])]:
+    with open(os.path.join(SELF_PATH, "examples/newsfeeds.pickle"), "rb") as f:
+        data = pickle.load(f)
+        return data
 
 
-@pytest.mark.usefixtures("database")
-@pytest.mark.parametrize("fst_part,scd_part", [newsfeeds()])
-def test_filling_database(fst_part: [News], scd_part: [News]):
+# todo convert this to many tests arranged chronologically.
+@pytest.mark.usefixtures("database", "newsfeeds")
+def test_filling_database(newsfeeds):
+    """
+    The test checks whether the replacement of news page stamps is executed correctly.
+
+    """
+    # Making the news set empty.
+    fst_set, scd_set = newsfeeds
+    orm.delete(i for i in SiteSnapshot)
+    assert len(orm.select(i for i in SiteSnapshot)) == 0
+    # Then, trying to add four news from the first set.
     td = datetime.now()
-    update_latest_post_urls(td, fst_part)
-    current_content = orm.select(i for i in SiteSnapshot if i.timestamp == td)
-    assert len(current_content) == len(fst_part)
+    fst_pt, scd_pt = fst_set
+    # Here, a data set of 26 news from four sources are added to the db.
+    update_latest_post_urls(td, scd_pt)
+    assert len(orm.select(i for i in SiteSnapshot)) == 26
+    # Here, however, the
+    update_latest_post_urls(td, fst_pt)
+    assert len(orm.select(i for i in SiteSnapshot)) == 4
+    fst_pt, scd_pt = scd_set
+    update_latest_post_urls(td, fst_pt)
+    assert len(orm.select(i for i in SiteSnapshot)) == len(fst_pt) + 2
+    update_latest_post_urls(td, scd_pt)
+    assert len(orm.select(i for i in SiteSnapshot)) == len(fst_pt) + len(scd_pt)
+
