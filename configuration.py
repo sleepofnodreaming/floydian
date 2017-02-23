e04c34b2aeb25516b8064711153da33fc30ee7b1
Marina Mazurova <sleepofnodreaming12@gmail.com>
2017-02-24 00:04:07 +0300

    configuration.py: validation added.

--- configuration.py
+++ configuration.py
@@ -1,5 +1,40 @@
 import json
 import os
 
+from jsonschema import validate
+
 SELF_PATH = os.path.dirname(os.path.realpath(__file__))
-SETTINGS = json.loads(open(os.path.join(SELF_PATH, "cfg.json")).read()) # todo add config validation.
+SETTINGS = json.loads(open(os.path.join(SELF_PATH, "cfg.json")).read())
+
+
+schema = {
+    "title": "Config",
+    "type": "object",
+    "properties": {
+        "translate-key": {
+            "type": "string",
+        },
+        "mailer": {
+            "type": "object",
+            "properties": {
+                    "server": {"type": "string"},
+                    "port": {"type": "number"},
+                    "sender": {"type": "string"},
+                },
+            "required": ["server", "port", "sender"],
+        },
+        "sendto": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                },
+                "minItems": 1,
+                "uniqueItems": True,
+            }
+
+    },
+    "required": ["mailer", "sendto"]
+}
+
+
+validate(SETTINGS, schema)
